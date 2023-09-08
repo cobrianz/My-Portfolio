f84ef360d33f06ad93c60dd9e655b64ca8d5a1a6
cobrianz <briancheruiyot.h@gmail.com>
2023-09-08 22:29:58 +0300

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './header.css';
-import logo from '../../assets/logo.png'
-import {links} from '../../data/links'
-import bars from '../../assets/baseline-density-small.png'
-import x from '../../assets/x.png'
-const header = () => {
+import logo from '../../assets/logo.png';
+import { links } from '../../data/links';
+import bars from '../../assets/baseline-density-small.png';
+import x from '../../assets/x.png';
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className='header'>
-        <nav>
-            <div className="logo">
-                <img src={logo} alt="" />
-            </div>
-            <ul className='menu__items'>
-                <li><a href="#home">{links[0]}</a></li>
-                <li><a href="#about">{links[1]}</a></li>
-                <li><a href="#work">{links[2]}</a></li>
-                <li><a href="#skills">{links[3]}</a></li>
-                <li><a href="#contact">{links[4]}</a></li>
-            </ul>
-        </nav>
-                <img src={bars} className='menu__btn' id='openButton' onClick={
-                    
-                } />
-                <img src={x} className='menu__btn' id='closeButton' alt="" />
-    </div>
-  )
-}
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        {isMenuOpen ? (
+          <ul className='menu__items'>
+            <li><a href="#home">{links[0]}</a></li>
+            <li><a href="#about">{links[1]}</a></li>
+            <li><a href="#work">{links[2]}</a></li>
+            <li><a href="#skills">{links[3]}</a></li>
+            <li><a href="#contact">{links[4]}</a></li>
+          </ul>
+        ) : null}
+      </nav>
+      <img
+        src={bars}
+        className='menu__btn'
+        id='openButton'
+        alt=""
+        onClick={toggleMenu}
 
-export default header
+      />
+      {isMenuOpen && (
+        <img
+          src={x}
+          className='menu__btn'
+          id='closeButton'
+          alt=""
+          onClick={toggleMenu}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Header;
