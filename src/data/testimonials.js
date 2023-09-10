a10723b2348a41712c3690f651f75bbcaf268b94
cobrianz <briancheruiyot.h@gmail.com>
2023-09-10 23:36:37 +0300

    commit

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,29 +1,29 @@
-import about1 from '../assets/about02.png';
-import about2 from '../assets/about04.png';
-import about3 from '../assets/about03.png';
-import about4 from '../assets/about01.png';
+import image1 from '../assets/avatar2.jpg';
+import image2 from '../assets/avatar3.jpg';
+import image3 from '../assets/avatar5.jpg';
+import image4 from '../assets/avatar8.jpg';
 
 export const testimonialsData = [
     {
-      image: about1,
+      image: image1,
       review: "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
       name: 'MATHEW HENDRICKSON',
       status : 'Samsung Kenya'
     },
     {
-      image: about2,
+      image: image2,
       review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
       name: 'JOHN KEVIN',
       status: 'Oracle Inc'
     },
     {
-      image : about3,
+      image : image3,
       review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
       name: ' Mary Dickson',
       status: "ALX, partner"
     },
     {
-      image : about4,
+      image : image4,
       review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
       name: 'Frankline Hubson',
       status: "Client, Freelancer"
